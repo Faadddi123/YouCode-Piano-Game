1509e4ffb03f869f0286fc4c0456460218791a09
siskoweb <sisko4dev@gmail.com>
2023-10-26 16:18:18 +0100

    create dunctiin play sound usieng press keys

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const pianoKeys = document.querySelectorAll(".piano-btns .btn")
-// volumeSlider = document.querySelector(".volume-slider input"),
-// keysCheckbox = document.querySelector(".keys-checkbox input");
+
 
 let allKeys = [],
     audio = new Audio(`sound/play.wav`);
 
+
+
 const playTune = (key) => {
-    audio.src = `sound/${key}.wav`; // passing audio src based on key pressed 
+    console.log("work")
+    audio.src = `sound/play.wav`; // passing audio src based on key pressed 
     audio.play(); // playing audio
 
     const clickedKey = document.querySelector(`[data-key="${key}"]`); // getting clicked key element
+    console.log(clickedKey)
     clickedKey.classList.add("active"); // adding active class to the clicked key element
     setTimeout(() => { // removing active class after 150 ms from the clicked key element
         clickedKey.classList.remove("active");
@@ -22,20 +25,14 @@ pianoKeys.forEach(key => {
     key.addEventListener("click", () => playTune(key.dataset.key));
 });
 
-const handleVolume = (e) => {
-    audio.volume = e.target.value; // passing the range slider value as an audio volume
-}
 
-const showHideKeys = () => {
-    // toggling hide class from each key on the checkbox click
-    pianoKeys.forEach(key => key.classList.toggle("hide"));
-}
+
 
 const pressedKey = (e) => {
     // if the pressed key is in the allKeys array, only call the playTune function
     if (allKeys.includes(e.key)) playTune(e.key);
 }
 
-keysCheckbox.addEventListener("click", showHideKeys);
-volumeSlider.addEventListener("input", handleVolume);
+
+
 document.addEventListener("keydown", pressedKey);
