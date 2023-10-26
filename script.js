3ceeb9e490db67fd1babe5ae8d0467688ffb5753
siskoweb <sisko4dev@gmail.com>
2023-10-26 16:45:30 +0100

    fix probelm two sounds should play same time

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 const pianoKeys = document.querySelectorAll(".piano-btns .btn") // getting all keys
 
 
-let allKeys = [],
-    audio = new Audio(`sound/play.wav`);
-
-
+let allKeys = []
 
 const playSound = (key) => {
-
+    audio = new Audio(`sound/${key}.wav`);
     audio.src = `sound/${key}.wav`; // passing audio src based on key pressed 
     audio.play(); // playing audio
 
     const clickedKey = document.querySelector(`[data-key="${key}"]`); // getting clicked key element
     console.log(clickedKey)
-    clickedKey.classList.add("active"); // adding active class to the clicked key element
+    // adding active class depend of wich key blakc or white
+    if (clickedKey.classList.contains("white")) clickedKey.classList.add("activeWhite");
+    else {
+        clickedKey.classList.add("activeBlack");
+    }
     setTimeout(() => { // removing active class after 150 ms from the clicked key element
-        clickedKey.classList.remove("active");
-    }, 150);
+        clickedKey.classList.remove("activeBlack");
+        clickedKey.classList.remove("activeWhite");
+    }, 200);
 }
 
 pianoKeys.forEach(key => {
     allKeys.push(key.dataset.key); // addz data-key value to the allKeys array
     // calling playSound function with passing data-key value as an argument
-    key.addEventListener("click", () => playSound(key.dataset.key));
+    key.addEventListener("keydown", () => playSound(key.dataset.key));
 });
 
 
@@ -31,9 +33,8 @@ pianoKeys.forEach(key => {
 const pressedKey = (e) => {
     //check if the kat passed is in allkey array , if yes call playsound fun
     if (allKeys.includes(e.key)) playSound(e.key);
-    console.log(`no sound belong this key #${e.key}`)
+    // console.log(`no sound belong this key #${e.key}`)
 }
-
 
 
 document.addEventListener("keydown", pressedKey);
